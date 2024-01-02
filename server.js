aaf92ab8205217f704e7796dff6113182649ad1f
Param Desai <93168030+ParamDesai111@users.noreply.github.com>
2024-01-02 15:55:12 -0500

    server problem

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 
-// Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
