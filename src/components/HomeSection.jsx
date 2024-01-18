12ef58e44390575a143773254424b52fc154dfc2
Param Desai <93168030+ParamDesai111@users.noreply.github.com>
2024-01-18 01:23:10 -0500

    Home Page Changes

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import faceImage from '../images/face.jpg'; // Import your face image
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'; // Import icons
 
 function HomeSection() {
   return (
@@ -6,7 +8,16 @@ function HomeSection() {
       <div className="row align-items-center">
         <div className="col-md-6">
           <h1>Hi, I'm Param Desai</h1>
-          <p>I'm a full stack engineer and a software developer with a passion to learn Artificial Intelligence and Machine Learning</p>
+          <p>I'm a full-stack engineer and a software developer with a passion to learn Artificial Intelligence and Machine Learning</p>
+          <div className="social-links">
+            <a href="https://github.com/yourusername" className="social-link"><FaGithub /></a>
+            <a href="https://linkedin.com/in/yourusername" className="social-link"><FaLinkedin /></a>
+            <a href="https://twitter.com/yourusername" className="social-link"><FaTwitter /></a>
+            {/* Add more social links if needed */}
+          </div>
+        </div>
+        <div className="col-md-6">
+          <img src={faceImage} alt="Param Desai" className="profile-image" />
         </div>
       </div>
     </section>
