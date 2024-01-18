33bbed6c8e8f65b5033a60d0b9fb78b94d062bc4
Param Desai <93168030+ParamDesai111@users.noreply.github.com>
2024-01-18 11:19:42 -0500

    Home page changes

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -9,15 +9,15 @@ function HomeSection() {
         <div className="col-md-6">
           <h1>Hi, I'm Param Desai</h1>
           <p>I'm a full-stack engineer and a software developer with a passion to learn Artificial Intelligence and Machine Learning</p>
+        </div>
+        <div className="col-md-6 text-center">
+          <img src={faceImage} alt="Param Desai" className="profile-image" />
           <div className="social-links">
             <a href="https://github.com/yourusername" className="social-link"><FaGithub /></a>
             <a href="https://linkedin.com/in/yourusername" className="social-link"><FaLinkedin /></a>
             <a href="https://twitter.com/yourusername" className="social-link"><FaTwitter /></a>
             {/* Add more social links if needed */}
           </div>
-        </div>
-        <div className="col-md-6">
-          <img src={faceImage} alt="Param Desai" className="profile-image" />
         </div>
       </div>
     </section>
