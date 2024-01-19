710c663e583548ce958fd6f3c47808b2cd50d540
Param Desai <93168030+ParamDesai111@users.noreply.github.com>
2024-01-19 17:27:09 -0500

    Images

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -7,6 +7,9 @@ function HomeSection() {
   return (
     <section id="home" className="container mt-5">
       <div className="row align-items-center">
+      <div className="col-md-6 text-center">
+          <img src={faceImage} alt="Param Desai" className="profile-image" />
+        </div>
         <div className="col-md-6">
           <h1>Hi, I'm Param Desai</h1>
           <p>I'm a full-stack engineer and a software developer with a passion to learn Artificial Intelligence and Machine Learning</p>
@@ -19,9 +22,7 @@ function HomeSection() {
             </div>
           </div>
         </div>
-        <div className="col-md-6 text-center">
-          <img src={faceImage} alt="Param Desai" className="profile-image" />
-        </div>
+        
       </div>
     </section>
   );
