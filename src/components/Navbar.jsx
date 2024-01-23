9908ffc7390e2ba8321bb975b8aa8c661064b9ae
Param Desai <93168030+ParamDesai111@users.noreply.github.com>
2024-01-23 00:01:36 -0500

    Navbar fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Navbar() {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
       <a className="navbar-brand" href="#home">Param Desai</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <button 
+        className={"navbar-toggler" + (isNavCollapsed ? '' : ' collapsed')} 
+        type="button" 
+        data-toggle="collapse" 
+        data-target="#navbarNav" 
+        aria-controls="navbarNav" 
+        aria-expanded={!isNavCollapsed} 
+        aria-label="Toggle navigation" 
+        onClick={handleNavCollapse}
+      >
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
+      <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item"><a className="nav-link" href="#home">Home</a></li>
           <li className="nav-item"><a className="nav-link" href="#experience">Experience</a></li>
           <li className="nav-item"><a className="nav-link" href="#projects">Projects</a></li>
           <li className="nav-item"><a className="nav-link" href="#about">About</a></li>
-
         </ul>
       </div>
     </nav>
