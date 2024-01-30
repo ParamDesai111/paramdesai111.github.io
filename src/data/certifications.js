ddb256794847e5d23f6dfa215807a76ec0691980
Param Desai <93168030+ParamDesai111@users.noreply.github.com>
2024-01-29 22:43:11 -0500

    pdf changes

---
+++ src/data/certifications.js
@@ -0,0 +1,11 @@
+const certifications = [
+    {
+      title: "PCEP – Certified Entry-Level Python Programmer",
+      imageUrl: '../images/certification.png', // Assuming certification.png is the image file
+      link: "https://verify.openedg.org/?id=bzCH.dXru.2Xw3"
+    },
+    // ... other certifications
+  ];
+  
+  export default certifications;
+  
