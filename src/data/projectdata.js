865461781dbf7a609185b3e2c7368892970320c7
Param Desai <93168030+ParamDesai111@users.noreply.github.com>
2024-08-14 01:43:10 +0000

    Projects Section changes

--- src/data/projectdata.js
+++ src/data/projectdata.js
@@ -7,34 +7,31 @@ const projectData = [
         live_url: "https://youtu.be/6PqKeg6QgxA",
         
     },
-
     {
         id: 2,
-        title: "Machine Learning Sudoku Solver",
-        tech: "Python pandas numpy TensorFlow Jupyter",
-        github_url: "https://github.com/ParamDesai111/Sudoku-Solver",
-
+        title: "Sports Analysis Data Pipeline",
+        tech: "Python Databricks AzureSQL Scikit-Learn Azure Data Factory React.js PowerBI  Algorithm: Random Forest Regressor",
+        github_url: "https://github.com/ParamDesai111/SportsAnalyticsPipeline"
     },
     {
         id: 3,
-        title: "Param's Portfolio Website",
-        tech: "React.js JavaScript HTML CSS Bootstrap",
-        github_url: "https://github.com/ParamDesai111/ParamDesai111.github.io",
-        live_url: "https://paramdesai.netlify.app/",
-    },
-    {
-        id:4,
         title:"Movie Recommendation System",
-        tech: "Python sklearn pandas Machine Learning",
+        tech: "Python sklearn pandas Algorithm: Content Based Filtering",
         github_url: "https://github.com/ParamDesai111/Movie-Reccomendation-System"
     },
     {
-        id: 5,
+        id: 4,
         title: "Tetris Game Project",
         tech: "HTML CSS JavaScript",
         github_url: "https://github.com/ParamDesai111/Tetris-Game-Project",
         live_url: "https://paramdesai111.github.io/Tetris-Game-Project/",
 
+    },
+    {
+        id: 5,
+        title: "Param's Online Clinic (Machine Learning Health Reommendation System",
+        tech: "Python Sckit-Learn pandas Jupyter Algorithm: K-Nearest-Neighbours",
+        github_url: "https://github.com/ParamDesai111/Health-Recommendation-System"
     },
     {
         id: 6,
